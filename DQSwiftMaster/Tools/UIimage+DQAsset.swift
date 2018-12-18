4bda3abf50f98a35fd48223d1ed178d919938f0b
liangdeqing <deqing_ios@163.com>
2018-12-18 15:56:45 +0800

    fix errors

---
+++ DQSwiftMaster/Tools/UIimage+DQAsset.swift
@@ -0,0 +1,23 @@
+//
+//  UIimage+DQAsset.swift
+//  DQSwiftMaster
+//
+//  Created by wondertek on 2018/12/18.
+//  Copyright © 2018年 deqing. All rights reserved.
+//
+
+typealias DQAsset = UIImage.Asset
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    enum Asset : String {
+        case Tab_first = "1"
+        case Tab_second = "2"
+        case Tab_third = "3"
+        case Tab_fourth = "4"
+    }
+    
+    
+}
